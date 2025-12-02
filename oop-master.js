61371446be320d369c4e5bfe36ca859661a82fc7
Bismit Wagle <bismitwagle@gmail.com>
2025-12-02 16:13:17 +0530

    Added Polymorphism OOPS Concept

--- oop-master.js
+++ oop-master.js
@@ -109,7 +109,32 @@ class CoffeMachine{
 let myMachine= new CoffeMachine()
 // console.log(myMachine.start());
 // console.log(myMachine.brewCoffee());
-console.log(myMachine.pressStartButton());
+// console.log(myMachine.pressStartButton());
+
+//Polymorphism
+
+class Bird{
+
+    fly(){
+
+        return` Flying...`
+    }
+}
+
+class Penguin extends Bird{
+    fly(){
+        return `Penguins Can't Fly`;
+    }
+}
+
+let bird = new Bird()
+let penguin = new Penguin()
+
+// console.log(bird.fly());
+// console.log(penguin.fly());
+
+
+
 
 
 
